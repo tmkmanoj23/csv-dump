c1f031a396deae751f1e1d937034fd4fb5694e77
Manoj <manoj.kumart@incture.com>
2017-03-03 01:33:06 +0530

    changed db name

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     database: {
-        name: "naukri",
+        name: "britannia",
         path: "mongodb://127.0.0.1:27017/",
         host: "127.0.0.1",
         port: "27017"
